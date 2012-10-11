4cc82f06451a5a2fdb5c97df4694aec3ad8a742d
Caleb Mingle <me@caleb.io>
2012-10-11 02:38:10 -0700

    moved ApplicationController#banned? below to protected.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,24 +1,22 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   layout :layout_by_resource
-
   before_filter :banned?
+
+  protected
+  def layout_by_resource
+    if devise_controller?
+      "logged_out"
+    else
+      "application"
+    end
+  end
 
   def banned?
     if current_user.present? && current_user.has_role?(:banned)
       sign_out current_user
       flash[:alert] = "This account has been suspended."
       redirect_to new_user_session_path
-    end
-  end
-
-  protected
-
-  def layout_by_resource
-    if devise_controller?
-      "logged_out"
-    else
-      "application"
     end
   end
 end
